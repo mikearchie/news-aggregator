188637b28112ca4f8e7d841dc419ca0700957d1d
jae1 <7904001@gmail.com>
2017-06-07 20:04:17 -0700

    removes unnecessary codes, adds go back button

--- news/feed_view.php
+++ news/feed_view.php
@@ -1,12 +1,11 @@
 <?php
-# '../' works for a sub-folder.  use './' for the root
-require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
+// '../' works for a sub-folder.  use './' for the root
+require '../inc_0700/config_inc.php'; //provides configuration, pathing, error handling, db credentials
 
 // check variable of item passed in - if invalid data, forcibly redirect back to survey-list.php page
 if (isset($_GET['id']) && (int)$_GET['id'] > 0) { // proper data must be on querystring
 	 $myID = (int)$_GET['id']; // Convert to integer, will equate to zero if fails
 } else {
-//	myRedirect(VIRTUAL_PATH . "surveys/survey-list.php");
 	header('Location:category_view.php');
 }
 
@@ -14,26 +13,29 @@ $myNews = new News($myID);
 
 $sql = "select * FROM ".PREFIX."NewsFeeds";
 $config->titleTag = 'News RSS';
-# END CONFIG AREA ----------------------------------------------------------
-get_header(); #defaults to theme header or header_inc.php
+// END CONFIG AREA ----------------------------------------------------------
+get_header(); // defaults to theme header or header_inc.php
 ?>
+
 <h3 align="center"><?=smartTitle();?></h3>
 
 <?php
 if ($myNews->isValid) {
 	$response = file_get_contents($myNews->request);
 	$xml = simplexml_load_string($response);
-	echo '<h2>' . $xml->channel->title . '</h2>';
+	echo '<h2>' . ucwords($xml->channel->title) . '</h2>';
 	foreach ($xml->channel->item as $story) {
-		echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
-		echo '<p>' . $story->description . '</p><br /><br />';
+		echo '<section>';
+		echo '<a href="' . $story->link . '">' . $story->title . '</a>'; 
+		echo $story->description;
+		echo '</section>';
 	}
-} else {#no records
+	echo '<a href="'.htmlspecialchars($_SERVER['HTTP_REFERER']).'">Go Back</a>';
+} else {//no records
     echo "<div align=center>There are currently no News</div>";
 }
 
-get_footer(); #defaults to theme footer or footer_inc.php
-
+get_footer(); // defaults to theme footer or footer_inc.php
 
 class News
 {
